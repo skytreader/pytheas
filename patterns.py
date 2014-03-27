0856c92e03132732c13080d2540fc899ccffb9cd
Chad Estioco <chadestioco@gmail.com>
2014-03-27 18:10:39 +0800

    Added a close method to Fetcher and Sender.
    For resource clean-up.

--- patterns.py
+++ patterns.py
@@ -3,9 +3,17 @@ class Fetcher(object):
     def fetch(self):
         """
         Fetch from whatever data store. Returns the appropriate object
-        rerpesentation of the fetched data.
+        rerpesentation of the fetched data. By convention, that data should be
+        passable to a Sender.send for sending.
         """
         raise NotImplementedError("This fetcher has not yet implemented this method.")
+    
+    def close(self):
+        """
+        Override this method for the clean-up steps when this fetcher is
+        disposed of.
+        """
+        pass
 
 class Sender(object):
     
@@ -14,3 +22,10 @@ class Sender(object):
         Sends the data provided as argument to wherever route.
         """
         raise NotImplementedError("This sender has not yet implemented this method.")
+
+    def close(self):
+        """
+        Override this method for the clean-up steps when this fetcher is
+        disposed of.
+        """
+        pass
