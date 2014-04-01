c93d54b4f40b1c7bbd7cba6873c1511b90758676
Chad Estioco <chadestioco@gmail.com>
2014-04-01 18:36:30 +0800

    Added methods for integration with gevent sockets.
    Sockets will be used for the external interface signals.

--- pytheas/daemon.py
+++ pytheas/daemon.py
@@ -1,5 +1,9 @@
+import gevent
 import daemon
 import time
+
+from gevent.lock import Semaphore
+from gevent.server import StreamServer
 
 class Pytheas(object):
     """
@@ -13,6 +17,40 @@ class Pytheas(object):
         self.__fetcher = fetcher
         self.__sender = sender
         self.sleep_time = sleep_time
+        self.__external_server = StreamServer(("127.0.0.0", 16981), self.__listen_external)
+        self.__ticket_counter = 1
+        self.__ticket_counter_lock = Semaphore()
+
+    def __sendfetch(self):
+        self.__sender.send(self.__fetcher.fetch())
+        gevent.sleep(1)
+
+    def __listen_external(self, socket, address):
+        """
+        Sends two messages to any external client that has issued a command:
+          - The first message is of the format "OK <ticketno>" where ticketno
+            is a unique number identifying this transaction.
+          - When it has finished processing the transaction and the processing
+            is successful, it will send "T <ticketno>". Otherwise, send
+            "F <ticketno>".
+
+        All messages (commands and replies) are terminated by newlines (`\n`).
+        """
+        sockfile = socket.makefile()
+
+        while True:
+            command = sockfile.readline()
+            if not command:
+                break
+            else:
+                ticketno = -1
+                self.__ticket_counter_lock.acquire()
+                ticketno = self.__ticket_counter
+                self.__ticket_counter += 1
+                self.__ticket_counter_lock.release()
+                sockfile.write("OK " + str(ticketno))
+                sockfile.write("T " + str(ticketno))
+                sockfile.flush()
 
     def run(self):
         with daemon.DaemonContext():
