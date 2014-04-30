36d4447ad372512d68ece1ef2234cd4c4191227f
Chad Estioco <chadestioco@gmail.com>
2014-04-30 13:53:51 +0800

    Added command interpreter modules.
    Will be used for interacting with a live running daemon.

---
+++ pytheas/command_interpreter.py
@@ -0,0 +1,46 @@
+from errors import InvalidCommandException
+
+import json
+
+class CommandInterpreter(object):
+    """
+    All command interpreters should extend from this class.
+    """
+
+    def interpret_command(self, data):
+        """
+        Performs some action based on the data.
+
+        data -- Assumed as raw from the socket. This method will perform the
+        necessary parsing.
+        """
+        raise NotImplementedException("This method should be implemented.")
+
+class PytheasCommandInterpreter(CommandInterpreter):
+    """
+    Pytheas commands deal with on-the-spot reconfiguration of daemon properties.
+    Pytheas commands have the following structure:
+        
+        {"set":<attribute>, "val":<new value>}
+    """
+    
+    NUMERIC = r"\d+"
+
+    VALID_ATTRIBUTES = set(("sleep_time",))
+    VALUE_VALIDATIONS = {"sleep_time": NUMERIC}
+    
+    
+    def __init__(self, daemon):
+        """
+        Needs access to the daemon that will be using it.
+        """
+        self.daemon = daemon
+   
+    def interpret_command(self, data):
+        command = json.loads(data)
+        set_key = command.get("set")
+        val_key = command.get("val")
+        
+        if set_key and val_key:
+            if set_key not in PytheasCommandInterpreter.VALID_ATTRIBUTES:
+                raise InvalidCommandException(data)
---
+++ pytheas/tests/command_interpreter_tests.py
@@ -0,0 +1,22 @@
+from pytheas.command_interpreter import PytheasCommandInterpreter
+from pytheas.errors import InvalidCommandException
+from pytheas.sfdaemon import Pytheas
+
+import unittest
+
+class CommandInterpreterTests(unittest.TestCase):
+    
+    def test_validation(self):
+        empty_set = '{}'
+        wrong_set = '{"set":"nonexistent", "val":1}'
+        wrong_val = '{"set":"sleep_time", "val":"wrong"}'
+
+        pci = PytheasCommandInterpreter(None)
+
+        with self.assertRaises(InvalidCommandException):
+            pci.interpret_command(empty_set)
+            pci.interpret_command(wrong_set)
+            pci.interpret_command(wrong_val)
+
+if __name__ == "__main__":
+    unittest.main()
